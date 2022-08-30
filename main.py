f3a4ac8eebfd59c1e9a6c36fcdb4dd7e601cf536
Stephanie Ali <sali.1444@eagle.fgcu.edu>
2022-08-29 22:23:12 -0400

    commit up to turning rect green when clicked

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+green = (0, 128, 0)
 
 # display size
 DISPLAY_WIDTH = 800
@@ -27,11 +28,18 @@ while not gameExit:
             gameExit = True
 
     gameDisplay.fill(white)     # display's color
-    pygame.draw.rect(gameDisplay, black, rect, 5)      # draw rectangle with (platform, color, where, outline width
+    pygame.draw.rect(gameDisplay, black, rect, 5)      # draw rectangle with (platform, color, where, outline width)
 
-
+    # turns rect red when mouse hovers the rect
     if (rect.collidepoint(pygame.mouse.get_pos())):
-       pygame.draw.rect(gameDisplay, red, rect, 5)  # draw rectangle with (platform, color, where, outline width
+        pygame.draw.rect(gameDisplay, red, rect, 5)
+        # turns rect green when clicked
+        pygame.event.get()
+        mouse = pygame.mouse.get_pressed()
+        if mouse[0]:    # 0 is left click
+            pygame.draw.rect(gameDisplay, green, rect, 5)
+    # move the mouse while holding left button and drag rect to desired location
+
 
     pygame.display.update()
 pygame.quit()
