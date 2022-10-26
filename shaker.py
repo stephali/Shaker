99d482997372c1dff6cd21a9800bfac149fa7f66
Stephanie Ali <sali.1444@eagle.fgcu.edu>
2022-10-26 01:38:22 -0400

    - See if we can simplify container.drawRed/Green() - Ready to begin collisions

--- shaker.py
+++ shaker.py
@@ -4,6 +4,7 @@ pygame.init()
 # game colors
 white = (255, 255, 255)
 black = (0, 0, 0)
+blue = (36, 160, 237)
 red = (255, 0, 0)
 green = (0, 128, 0)
 
@@ -13,11 +14,34 @@ DISPLAY_HEIGHT = 800
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption('Shaker')
 
-# create rect object with (position), (width & length)
-rectWidth = 200
-rectHeight = 200
-rect = pygame.Rect(((DISPLAY_WIDTH / 2) - rectWidth / 2, (DISPLAY_HEIGHT / 2) - rectHeight / 2),
-                   (rectWidth, rectHeight))
+
+class Container:
+    def __init__(self):
+        # create rectangular object with (position), (width & length)
+        self.rectWidth = 200
+        self.rectHeight = 200
+        self.rect = pygame.Rect(((DISPLAY_WIDTH / 2) - self.rectWidth / 2, (DISPLAY_HEIGHT / 2) - self.rectHeight / 2), (self.rectWidth, self.rectHeight))
+
+    def draw(self):
+        pygame.draw.rect(gameDisplay, black, self.rect, 5)
+
+    def drawRed(self):
+        pygame.draw.rect(gameDisplay, red, self.rect, 5)
+
+    def drawGreen(self):
+        pygame.draw.rect(gameDisplay, green, self.rect, 5)
+
+    def move(self):
+        self.rect.center = event.pos
+
+
+class Bead:
+    def draw(self):
+        pygame.draw.circle(gameDisplay, blue, (400, 400), 20)
+
+
+container = Container()
+bead = Bead()
 
 gameExit = False
 
@@ -28,23 +52,26 @@ while not gameExit:
             gameExit = True
 
     gameDisplay.fill(white)  # display's color
-    pygame.draw.rect(gameDisplay, black, rect, 5)  # draw rectangle (platform, color, where, outline width)
+
+    # draw bead
+    bead.draw()
+
+    # draw container (platform, color, where, outline width)
+    container.draw()
 
     # turns rect red when mouse collides with the rect
-    if rect.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(gameDisplay, red, rect, 5)
+    if container.rect.collidepoint(pygame.mouse.get_pos()):
+        container.drawRed()
 
         # turns rect green when clicked
         pygame.event.get()  # need this for left click to work
         click = pygame.mouse.get_pressed()
         if click[0]:  # 0 is left click
-            pygame.draw.rect(gameDisplay, green, rect, 5)
+            container.drawGreen()
 
             # move the mouse while holding left button and drag rect to desired location
             if event.type == pygame.MOUSEMOTION:
-                rect.center = event.pos  # this works but cursor moves to rect's center
-                # ---test 1--- x, y = pygame.mouse.get_pos()
-                # ---test 1--- rect.center = (((x - rect.centerx) + x), ((y - rect.centery) + y))
+                container.move()
 
     pygame.display.update()
 pygame.quit()
